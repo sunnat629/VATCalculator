34c105e40899edd8e7db793ee367731eb23796f8
S M Mohi Us Sunnat <suncha629@gmail.com>
2019-03-22 15:54:13 +0600

    Initial commit

---
+++ app/src/main/java/com/sunnat629/vatcalculator/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sunnat629.vatcalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
